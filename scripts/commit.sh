d36a79a9c730ce0bd5f89ff2e76445d12aff007a
Rick Waldron <waldron.rick@gmail.com>
2015-02-16 11:10:18 -0500

    Adds "tldr" to commit message.
    Example:
    
    $ /scripts/commit.sh "array concat arraylikes"
    
    Import tests from Google V8 (array concat arraylikes)
    These tests are derived from the following files within the Google V8 project:
    
    $ /scripts/commit.sh
    Import tests from Google V8
    These tests are derived from the following files within the Google V8 project:

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -2,10 +2,16 @@
 
 # Prepare a commit message that includes a listing of files removed from V8
 
+TLDR=""
+
+if [[ "$1" != "" ]]; then
+  TLDR="($1)"
+fi
+
 FILES=$(cd v8-git-mirror; git diff master --name-status | sed 's/^[^D].*$//g' | sed 's/^D\s*//g')
 
 MESSAGE=$(cat <<EOF
-Import tests from Google V8
+Import tests from Google V8 $TLDR
 
 These tests are derived from the following files within the Google V8
 project:
