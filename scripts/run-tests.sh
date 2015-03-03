51bf713847b421ea2f2dfa117dbfebc34bafaea6
Mike Pennisi <mike@mikepennisi.com>
2015-03-03 16:44:21 -0500

    Fix typo

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ LOCAL_V8=v8-git-mirror/out/native/d8
 
 function help() {
   cat <<HELP
-Usage: run-new-tests.sh [RUNTIME]
+Usage: run-tests.sh [RUNTIME]
 Execute the "new" Test262 tests (any files in the current branch but not in
 master). In the absense of a specific RUNTIME, the locally-built version of V8
 will be used.
